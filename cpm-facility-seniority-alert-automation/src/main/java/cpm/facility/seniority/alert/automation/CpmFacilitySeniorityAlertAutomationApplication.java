1cb64d54e75230b6e440fbb60647f15d6386d102
Bholanath <bholanath.das21@gmail.com>
2021-08-12 18:25:11 +0530

    Add files via upload

---
+++ cpm-facility-seniority-alert-automation/src/main/java/cpm/facility/seniority/alert/automation/CpmFacilitySeniorityAlertAutomationApplication.java
@@ -0,0 +1,13 @@
+package cpm.facility.seniority.alert.automation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CpmFacilitySeniorityAlertAutomationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CpmFacilitySeniorityAlertAutomationApplication.class, args);
+	}
+
+}
---
+++ cpm-facility-seniority-alert-automation/src/test/java/cpm/facility/seniority/alert/automation/CpmFacilitySeniorityAlertAutomationApplicationTests.java
@@ -0,0 +1,13 @@
+package cpm.facility.seniority.alert.automation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CpmFacilitySeniorityAlertAutomationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
